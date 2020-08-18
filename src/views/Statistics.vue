dc665304272d618cb6d53ad33eeb46de2758da74
ppambler <allinlbj@gmail.com>
2020-08-18 11:36:38 +0800

    重复三次布局

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    Statistics
+  <div class="nav-wrapper">
+    <div class="content">Statistics</div>
     <Nav />
   </div>
 </template>
@@ -12,4 +12,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid yellow;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  border: 1px solid lightcoral;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
