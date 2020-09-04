c1036d10d1c607206daedf2a2b0a8c2a8a23203f
ppambler <allinlbj@gmail.com>
2020-09-04 16:28:25 +0800

    收集四个组件的数据到父组件的 record

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -26,7 +26,8 @@ import { Component, Prop } from "vue-property-decorator";
 
 @Component
 export default class NumberPad extends Vue {
-  output = "0";
+  @Prop() readonly value!: number;
+  output = this.value.toString();
   inputContent(event: MouseEvent) {
     const button = event.target as HTMLButtonElement;
     const input = button.textContent!;
@@ -56,7 +57,9 @@ export default class NumberPad extends Vue {
   clear() {
     this.output = "0";
   }
-  ok() {}
+  ok() {
+    this.$emit("update:value", this.output);
+  }
 }
 </script>
 
