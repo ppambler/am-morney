ad66dad899baddf1e5e9335c2cd15d1f5c903bc5
ppambler <allinlbj@gmail.com>
2020-10-05 21:43:44 +0800

    在 Money.vue 里边使用 Tabs

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord" />
-    <Types :value.sync="record.type" />
+    <Tabs :data-source="recordTypeList" :value.sync="record.type" />
     <div class="notes">
       <FormItem
         field-name="备注"
@@ -16,15 +16,16 @@
 <script lang="ts">
 import Vue from "vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
-import Types from "@/components/Money/Types.vue";
 import FormItem from "@/components/Money/FormItem.vue";
 import Tags from "@/components/Money/Tags.vue";
+import Tabs from "@/components/Tabs.vue";
 import { Component } from "vue-property-decorator";
+import recordTypeList from "@/constants/recordTypeList";
 
 @Component({
   components: {
     NumberPad,
-    Types,
+    Tabs,
     FormItem,
     Tags,
   },
@@ -33,6 +34,7 @@ export default class Money extends Vue {
   get recordList() {
     return this.$store.state.recordList;
   }
+  recordTypeList = recordTypeList;
   record: RecordItem = {
     tags: [],
     notes: "",
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,10 @@
 <template>
   <Layout>
-    <Tabs class-prefix="type" :data-source="typeList" :value.sync="type" />
+    <Tabs
+      class-prefix="type"
+      :data-source="recordTypeList"
+      :value.sync="type"
+    />
     <Tabs
       class-prefix="interval"
       :data-source="intervalList"
@@ -19,6 +23,8 @@ import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import Types from "@/components/Money/Types.vue";
 import Tabs from "@/components/Tabs.vue";
+import intervalList from "@/constants/intervalList";
+import recordTypeList from "@/constants/recordTypeList";
 
 @Component({
   components: { Tabs, Types },
@@ -26,15 +32,8 @@ import Tabs from "@/components/Tabs.vue";
 export default class Statistics extends Vue {
   type = "-";
   interval = "day";
-  intervalList = [
-    { text: "按天", value: "day" },
-    { text: "按周", value: "week" },
-    { text: "按月", value: "month" },
-  ];
-  typeList = [
-    { text: "支出", value: "-" },
-    { text: "收入", value: "+" },
-  ];
+  intervalList = intervalList;
+  recordTypeList = recordTypeList;
 }
 </script>
 
