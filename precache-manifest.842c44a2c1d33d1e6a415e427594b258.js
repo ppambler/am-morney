fe9b1b52afeb3a35526cce0525a6255e05326391
ppambler <allinlbj@gmail.com>
2020-10-17 02:28:47 +0800

    deploy

---
+++ precache-manifest.842c44a2c1d33d1e6a415e427594b258.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74b689f05fc98e61948",
+    "url": "/vue-morney/css/app.606fb753.css"
+  },
+  {
+    "revision": "ce92c907f87b5c2cbd54490440da02f8",
+    "url": "/vue-morney/index.html"
+  },
+  {
+    "revision": "f74b689f05fc98e61948",
+    "url": "/vue-morney/js/app.06cefaca.js"
+  },
+  {
+    "revision": "360b62a06187994c2157",
+    "url": "/vue-morney/js/chunk-vendors.e2ef5204.js"
+  },
+  {
+    "revision": "1ae4a122208a71679b4804e8acb0c9b6",
+    "url": "/vue-morney/manifest.json"
+  },
+  {
+    "revision": "bb18bd8fc48f9feb78c462f80d487a7f",
+    "url": "/vue-morney/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-morney/robots.txt"
+  }
+]);
